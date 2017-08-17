14cca8c5d23c502d197473d664086f64bb725c4f
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-17 21:28:46 +0900

    feat(demo): update demo for newer extension version

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+'use strict'
+
 require('dotenv').load({ silent: true })
 
-const path   = require('path')
-const Mozaik = require('mozaik')
+const path = require('path')
+const Mozaik = require('@mozaik/server')
 
+let configFile = process.argv[2] || 'config.yml'
 
-Mozaik.configureFromFile(path.join(__dirname, 'config.yml'))
-    .then(() => {
-        Mozaik.registerApi('travis', require('mozaik-ext-travis/client'))
+console.log(`> using config file: '${configFile}'\n`)
 
+Mozaik.configureFromFile(path.join(__dirname, configFile))
+    .then(config => {
+        Mozaik.registerApi('travis', require('@mozaik/ext-travis/client'))
         Mozaik.start()
     })
     .catch(err => {
--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
-import 'mozaik/ui.css'
-import 'font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './register_themes'
+import './register_extensions'
+import Mozaik from '@mozaik/ui'
 
-import React         from 'react'
-import { render }    from 'react-dom'
-import Mozaik, { Registry, ThemeManager } from 'mozaik/ui'
-
-import travis        from 'mozaik-ext-travis'
-
-import solarizedDark from 'mozaik-themes/themes/solarized-dark'
-import nightBlue     from 'mozaik-themes/themes/night-blue'
-import sunny         from 'mozaik-themes/themes/sunny'
-import wine          from 'mozaik-themes/themes/wine'
-import snow          from 'mozaik-themes/themes/snow'
-import mini          from 'mozaik-themes/themes/mini'
-import miniKuro      from 'mozaik-themes/themes/mini-kuro'
-
-import 'mozaik-themes/themes/solarized-dark.css'
-import 'mozaik-themes/themes/night-blue.css'
-import 'mozaik-themes/themes/sunny.css'
-import 'mozaik-themes/themes/wine.css'
-import 'mozaik-themes/themes/snow.css'
-import 'mozaik-themes/themes/mini.css'
-import 'mozaik-themes/themes/mini-kuro.css'
-
-ThemeManager.add(solarizedDark)
-ThemeManager.add(nightBlue)
-ThemeManager.add(sunny)
-ThemeManager.add(wine)
-ThemeManager.add(snow)
-ThemeManager.add(mini)
-ThemeManager.add(miniKuro)
-
-ThemeManager.defaultTheme = nightBlue.name
-
-Registry.addExtensions({ travis })
-
-render(
-    <Mozaik />,
-    document.getElementById('mozaik')
-)
+ReactDOM.render(<Mozaik />, document.getElementById('root'))
